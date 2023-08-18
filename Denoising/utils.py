608555ae4c3a3b9b2dbf7f81bc20d270052c0108
alsco1234 <alsco4321@gmail.com>
2023-08-18 09:33:57 +0900

    [UPDATE]

--- Denoising/utils.py
+++ Denoising/utils.py
@@ -66,18 +66,40 @@ def to_sequence(_Patches):
 # 03. PATCHING AND MERGING
 ##########################################
 def divide_patches(image, patch_size=256, overlap_size=4):
+    """
+    DIVIDE IMAGES INTO PATHCES
+    THIS PATCEHS MUST HAVE DUPLICARTED AREA
+    
+    [INPUT]
+    image: (H, W, C) : numpy array
+    patch_size : N : default = 256
+    overlap_size : duplication factor : default = 4
+    
+    [OUTPUT]
+    patches: (X, Y, PH, PW, C) : numpy array
+    """
+    
+    # Calculate the step size for each axis
     step_size = patch_size - overlap_size
-
+    
+    # Calculate the number of patches along each axis
     x_axis_patches = np.ceil((image.shape[0] - overlap_size) / step_size).astype(int)
     y_axis_patches = np.ceil((image.shape[1] - overlap_size) / step_size).astype(int)
-
+    
+    # Calculate the size of the padded image
     padded_image_height = (x_axis_patches - 1) * step_size + patch_size
     padded_image_width = (y_axis_patches - 1) * step_size + patch_size
     
-    padded_image = np.pad(image, ((0, padded_image_width), (0, padded_image_height)), mode='reflect')
+    # Initialize the padded image array with mirror padding
+    padded_image = np.pad(image, ((0, padded_image_width), (0, padded_image_height), (0, 0)), mode='reflect')
+    print("mode is mirror")
+    # Copy the input image into the padded image array
     padded_image[:image.shape[0], :image.shape[1]] = image
-    patches = np.zeros((x_axis_patches, y_axis_patches, patch_size, patch_size))
     
+    # Initialize the patches array
+    patches = np.zeros((x_axis_patches, y_axis_patches, patch_size, patch_size, padded_image.shape[2]))
+    
+    # Iterate over the patches and extract them from the padded image
     for i in range(x_axis_patches):
         for j in range(y_axis_patches):
             x_start = i * step_size
@@ -88,62 +110,62 @@ def divide_patches(image, patch_size=256, overlap_size=4):
     return patches
 
 
-def concat_h(img1, img2):
+def concat_h(img1, img2, overlap_size=4):
     for x in range(0, img1.shape[0]):
         for y in range(0, img1.shape[1]):
-            if y > img1.shape[1]-85:
-                img1[x][y] = img1[x][y] * (-(y-img1.shape[1]-1)/85) ## -1하는게 보기에 제일 ㄱㅊ았음
+            if y > img1.shape[1]-overlap_size:
+                img1[x][y] = img1[x][y] * (-(y-img1.shape[1]-1)/overlap_size)
                 
     for x in range(0, len(img2)):
         for y in range(0, len(img2)):
-            if y < 85:
-                img2[x][y] = img2[x][y] * ((y)/85) ## 예를 y+1하는게 논리적으론 맞는데.. 보기에 이상해짐 ㅠ
+            if y < overlap_size:
+                img2[x][y] = img2[x][y] * ((y)/overlap_size) 
                 
-    img3 = np.zeros((img2.shape[0], img1.shape[1]+img2.shape[0]-85), np.uint8)
+    img3 = np.zeros((img2.shape[0], img1.shape[1]+img2.shape[0]-overlap_size), np.uint8)
     for x in range(0, img2.shape[0]):
-        for y in range(0, img1.shape[1]+img2.shape[0]-85):
+        for y in range(0, img1.shape[1]+img2.shape[0]-overlap_size):
             if y < img1.shape[1]:
                 img3[x][y] += img1[x][y]
                 
-            if y > (img1.shape[1]-85):
-                img3[x][y] = np.clip(img3[x][y] + img2[x][y - (img1.shape[1] - 85)], 0.0, 255.0)
+            if y > (img1.shape[1]-overlap_size):
+                img3[x][y] = np.clip(img3[x][y] + img2[x][y - (img1.shape[1] - overlap_size)], 0.0, 255.0)
           
     return img3
 
         
-def concat_v(img1, img2):
+def concat_v(img1, img2, overlap_size=4):
     for x in range(0, img1.shape[0]):
         for y in range(0, img1.shape[1]):
-            if x > img1.shape[0] - 85:
-                img1[x][y] = img1[x][y] * (-(x - img1.shape[0]-1) / 85) ##
+            if x > img1.shape[0] - overlap_size:
+                img1[x][y] = img1[x][y] * (-(x - img1.shape[0]-1) / overlap_size) ##
 
     for x in range(0, len(img2)):
         for y in range(0, len(img2[0])):
-            if x < 85:
-                img2[x][y] = img2[x][y] * (x / 85)
+            if x < overlap_size:
+                img2[x][y] = img2[x][y] * (x / overlap_size)
 
-    img3 = np.zeros((img1.shape[0] + img2.shape[0] - 85, img1.shape[1]), np.uint8)
-    for x in range(0, img1.shape[0] + img2.shape[0] - 85):
+    img3 = np.zeros((img1.shape[0] + img2.shape[0] - overlap_size, img1.shape[1]), np.uint8)
+    for x in range(0, img1.shape[0] + img2.shape[0] - overlap_size):
         for y in range(0, img1.shape[1]):
             if x < img1.shape[0]:
                 img3[x][y] += img1[x][y]
 
-            if x > (img1.shape[0] - 85):
-                img3[x][y] = np.clip(img3[x][y] + img2[x - (img1.shape[0] - 85)][y], 0.0, 255.0)
+            if x > (img1.shape[0] - overlap_size):
+                img3[x][y] = np.clip(img3[x][y] + img2[x - (img1.shape[0] - overlap_size)][y], 0.0, 255.0)
     return img3
 
 
-def merge_patches(patches):
+def merge_patches(patches, overlap_size=4):
     horizontal_concatenated = []
     for i in range(patches.shape[0]):
         row = patches[i][0]
         for j in range(1, patches.shape[1]):
-            row = concat_h(row, patches[i][j])
+            row = concat_h(row, patches[i][j], overlap_size=4)
         horizontal_concatenated.append(row)
 
     final_image = horizontal_concatenated[0]
     for i in range(1, len(horizontal_concatenated)):
-        final_image = concat_v(final_image, horizontal_concatenated[i])
+        final_image = concat_v(final_image, horizontal_concatenated[i], overlap_size=4)
         
     return final_image
 
